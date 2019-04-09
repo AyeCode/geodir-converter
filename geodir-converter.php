85a0d397f86496a3e5716bab638528de1545fe9d
Naveen giri <1naveengiri@gmail.com>
2019-04-09 20:17:42 +0530

    Add Initial commit

---
+++ geodir-converter.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This is the main GeoDirectory Converter plugin file, here we declare and call the important stuff
+ *
+ * @package           Geodir_Converter
+ * @copyright         2016 AyeCode Ltd
+ * @license           GPLv3
+ * @since             1.0.0
+ *
+ * @geodir_claim_listing
+ * Plugin Name: GeoDirectory Converter
+ * Plugin URI: https://ayecode.com
+ * Description: A plugin to convert other directories to GeoDirectory
+ * Version: 1.0.0
+ * Author: AyeCode Ltd
+ * Author URI: https://wpgeodirectory.com/
+ * Requires at least: 4.9
+ * Tested up to: 5.1
+ * License: GPLv3
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: geodir-converter
+ * Domain Path: /languages
+ * Update URL: https://wpgeodirectory.com
+ */
